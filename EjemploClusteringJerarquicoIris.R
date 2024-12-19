242ede0e1e908f8aca5d6b97373d3f507d52973a
Sara Visoso <90227855+saravisoso@users.noreply.github.com>
2024-12-18 23:11:44 -0600

    Add files via upload
    Los datos iris que subimos son para este ejemplo. Es necesario descargarlos para que esto corra

---
+++ EjemploClusteringJerarquicoIris.R
@@ -0,0 +1,25 @@
+load("iris.rda")
+# Eliminar la columna "type" que no aporta información numérica
+iris_num <- iris[, -which(names(iris) == "type")]
+
+# Calcular la matriz de distancias
+dist_matrix <- dist(iris_num)
+
+# Realizar clustering aglomerativo y divisivo
+
+# Single Linkage
+hc_single <- agnes(dist_matrix, method = "single")
+plot(hc_single, main = "Single Linkage", which.plots = 2)
+
+# Complete Linkage
+hc_complete <- agnes(dist_matrix, method = "complete")
+plot(hc_complete, main = "Complete Linkage", which.plots = 2)
+
+# Average Linkage
+hc_average <- agnes(dist_matrix, method = "average")
+plot(hc_average, main = "Average Linkage", which.plots = 2)
+
+# Clustering divisivo
+hc_divisive <- diana(dist_matrix)
+plot(hc_divisive, main = "Clustering Divisivo", which.plots = 2)
+
